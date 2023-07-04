9a4377d836ead7f4009c4d7118d81e6a327503b0
Rafael Gontijo Ferreira <79460524+Gontijo8199@users.noreply.github.com>
2023-07-04 10:34:30 -0300

    Added files through upload
    fixing java directory tree

---
+++ Lusofa-update/src/main/java/com/mycompany/lusofa/update/LusofaUpdate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lusofa.update;
+
+/**
+ *
+ * @author rafer
+ */
+public class LusofaUpdate {
+
+    public static void main(String[] args) {
+        Seguranca.Mysql("select * from user;");
+    }
+}
